c252f4cbc3a0b376ce37d445a946931518507b47
felipeacguedes <felipe.acguedes@gmail.com>
2026-02-19 18:14:35 -0300

    configuring user controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { prisma } from '../lib/prisma';
+
+export async function createUser(req: Request, res: Response) {
+    try {
+        const { name, email } = req.body;
+
+        const user = await prisma.user.create({
+            data: { name, email }
+        });
+
+        return res.status(201).json(user);
+    }
+    catch (error) {
+        return res.status(400).json({ error: 'Error creating user'});
+    }
+}
+
+export async function getUsers(req: Request, res: Response) {
+    const users = await prisma.user.findMany();
+    return res.json(users);
+}
